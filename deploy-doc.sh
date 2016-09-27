96e59a1daf8e2077ee20a2ab12fb7cafc6431425
Erwan Loisant <eloisant@gmail.com>
2016-09-27 10:47:28 +0200

    Documentation in the docs folder

--- deploy-doc.sh
+++
@@ -1,9 +0,0 @@
-yard && \
-cd doc && \
-rm -rf doc/phpdoc-cache-* && \
-git init . && \
-git add . && \
-git commit -m "Update documentation."; \
-git push "git@github.com:prismicio/ruby-kit.git" master:gh-pages --force && \
-rm -rf .git
-
